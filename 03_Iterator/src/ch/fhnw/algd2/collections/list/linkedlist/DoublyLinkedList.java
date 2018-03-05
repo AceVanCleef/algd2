3d81827fc35d52ca55dcdd9d138a53c90663aabc
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2018-03-05 10:27:21 +0100

    import commit

---
+++ 03_Iterator/src/ch/fhnw/algd2/collections/list/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,231 @@
+package ch.fhnw.algd2.collections.list.linkedlist;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+import ch.fhnw.algd2.collections.list.MyAbstractList;
+
+public class DoublyLinkedList<E> extends MyAbstractList<E> {
+	// variable int modCount already declared by AbstractList<E>
+	private int size = 0;
+	private Node<E> first;
+
+	private Node<E> last;
+
+	@Override
+	public boolean add(E e) {
+		if (e == null) throw new NullPointerException("null not allowed");
+		Node<E> n = new Node<>(e);
+		if (size == 0){
+			first = n;
+		} else if (size > 0) {
+			last.next = n;
+		}
+		last = n;
+		++size;
+		return true;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (o == null) throw new NullPointerException("null not allowed");
+		Node<E> current = first;
+		while (current != null && !(current.elem.equals(o))){
+			current = current.next;
+		}
+		return current != null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (o == null) throw new NullPointerException("null not allowed");
+		if (!contains(o)) return false;
+		Node<E> current = first;
+		Node<E> previous = null;
+		while (current != null && !(current.elem.equals(o))){
+			previous = current;
+			current = current.next;
+		}
+		if (first == current) {				//special case: first element has to be removed
+			first = current.next;
+		} else if(last == current) {		//special case: last element has to be removed
+			previous.next = null;
+			last = previous;
+		} else {
+			previous.next = current.next;	//redirect reference ([a]->[b]->[c] zu [a]->[c]: Überbrückt B.)
+		}
+
+		--size;
+		return true;
+	}
+
+	@Override
+	public E get(int index) {
+		if (index >= size || index < 0) throw new IndexOutOfBoundsException("invalid index: too low or high");
+		if (size == 0)	throw new IndexOutOfBoundsException("empty list");			//case: empty list
+		//Note: 'size !=  0' is guaranteed from here on.
+		if (index == 0){ 			//case: get first element.
+			return first.elem;
+		} else if (index == size - 1) {			//case: get last element
+			return last.elem;
+		}
+		//moving through the list
+		Node<E> current = first;
+		int i = 0;
+		while (i != index) { //No guard such as 'i < size' or 'current!= null' needed. Valid index range already tested.
+			current = current.next;
+			++i;
+		}
+		return current.elem;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		if (element == null) throw new NullPointerException("null not allowed");
+		if (index > size || index < 0) throw new IndexOutOfBoundsException("Index too large or small");
+		if (size == 0){						//case: empty list
+			first = new Node<>(element);
+			last = first;
+			++size;
+			return;
+		}
+		else if (index == size) {			//case: adding as last element
+			last.next = new Node<>(element);
+			last = last.next;
+			++size;
+			return;
+		}
+		//moving through the list
+		Node<E> current = first;
+		Node<E> previous = null;
+		int i = 0;
+		while (i != index) {	//No guard such as 'current != null' needed. Valid index range already tested.
+			previous = current;
+			current = current.next;
+			++i;
+		}
+		Node<E> n = new Node<>(previous, element, current);
+		if (i == 0){					//case: not empty list, but adding as first element
+			first = n;
+		} else {						//case: adding in-between
+			previous.next = n;
+		}
+		last = n;
+		++size;
+	}
+
+	//deletion: [A]->[B]->[C]-| ; [A]->[C]
+	@Override
+	public E remove(int index) {
+		if (index >= size || index < 0) throw new IndexOutOfBoundsException("invalid index: too low or high");
+		if (size == 0)	throw new IndexOutOfBoundsException("empty list");			//case: empty list
+		//Note: 'size !=  0' is guaranteed from here on.
+		if (index == 0){ 				//case: get first element
+			Node<E> oldFirst = first;
+			first = first.next;
+			--size;
+			return oldFirst.elem;
+		}
+		//moving through the list
+		Node<E> current = first;
+		Node<E> previous = null;
+		int i = 0;
+		while (i != index) {	//No guard such as 'current != null' or 'i < size' needed. Valid index range already tested.
+			previous = current;
+			current = current.next;
+			++i;
+		}
+		previous.next = current.next; //removes element at index
+		if (i == size - 1) {			//case: get last element
+			//Note: previous.next will be automatically equal null.
+			// 	Reason: 'previous = current' && 'previous.next = current.next', current is last elem. and 'current.next == null'
+			last = previous; //last.next will be null because of 'previous.next == null';
+		}
+		--size;
+		return current.elem;
+	}
+
+	@Override
+	public Object[] toArray() {
+		return arrayForDoublyLinkedList();
+		// return arrayForCyclicDoublyLinkedList();
+	}
+
+	private Object[] arrayForDoublyLinkedList() {
+		Object[] array = new Object[size];
+		int index = 0;
+		Node<E> current = first;
+		while (current != null) {
+			array[index++] = current.elem;
+			current = current.next;
+		}
+		return array;
+	}
+
+	private Object[] arrayForCyclicDoublyLinkedList() {
+		Object[] array = new Object[size];
+		int index = 0;
+		Node<E> current = first.next;
+		while (current != first) {
+			array[index++] = current.elem;
+			current = current.next;
+		}
+		return array;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new MyListIterator();
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		throw new UnsupportedOperationException("Implement later");
+	}
+
+	private static class Node<E> {
+		private E elem;
+		private Node<E> prev, next;
+
+		private Node(E elem) {
+			this.elem = elem;
+		}
+
+		private Node(Node<E> prev, E elem, Node<E> next) {
+			this.prev = prev;
+			this.elem = elem;
+			this.next = next;
+		}
+	}
+
+	private class MyListIterator implements Iterator<E> {
+		@Override
+		public boolean hasNext() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public E next() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	public static void main(String[] args) {
+		DoublyLinkedList<Integer> list = new DoublyLinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(Arrays.toString(list.toArray()));
+	}
+}
---
+++ 03_Iterator/src/ch/fhnw/algd2/collections/list/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,210 @@
+package ch.fhnw.algd2.collections.list.linkedlist;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import ch.fhnw.algd2.collections.list.MyAbstractList;
+
+public class SinglyLinkedList<E> extends MyAbstractList<E> {
+	// variable int modCount already declared by AbstractList<E>
+
+	private int size = 0;
+	private Node<E> first;
+
+	private Node<E> last;
+
+	@Override
+	public boolean add(E e) {
+		if (e == null) throw new NullPointerException("null not allowed");
+		Node<E> n = new Node<>(e);
+		if (size == 0){
+			first = n;
+		} else if (size > 0) {
+			last.next = n;
+		}
+		last = n;
+		++size;
+		return true;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (o == null) throw new NullPointerException("null not allowed");
+		Node<E> current = first;
+		while (current != null && !(current.elem.equals(o))){
+			current = current.next;
+		}
+		return current != null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (o == null) throw new NullPointerException("null not allowed");
+		if (!contains(o)) return false;
+		Node<E> current = first;
+		Node<E> previous = null;
+		while (current != null && !(current.elem.equals(o))){
+			previous = current;
+			current = current.next;
+		}
+		if (first == current) {				//special case: first element has to be removed
+			first = current.next;
+		} else if(last == current) {		//special case: last element has to be removed
+			previous.next = null;
+			last = previous;
+		} else {
+			previous.next = current.next;	//redirect reference ([a]->[b]->[c] zu [a]->[c]: Überbrückt B.)
+		}
+
+		--size;
+		return true;
+	}
+
+	@Override
+	public E get(int index) {
+		if (index >= size || index < 0) throw new IndexOutOfBoundsException("invalid index: too low or high");
+		if (size == 0)	throw new IndexOutOfBoundsException("empty list");			//case: empty list
+		//Note: 'size !=  0' is guaranteed from here on.
+		if (index == 0){ 			//case: get first element.
+			return first.elem;
+		} else if (index == size - 1) {			//case: get last element
+			return last.elem;
+		}
+		//moving through the list
+		Node<E> current = first;
+		int i = 0;
+		while (i != index) { //No guard such as 'i < size' or 'current!= null' needed. Valid index range already tested.
+			current = current.next;
+			++i;
+		}
+		return current.elem;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		if (element == null) throw new NullPointerException("null not allowed");
+		if (index > size || index < 0) throw new IndexOutOfBoundsException("Index too large or small");
+		if (size == 0){						//case: empty list
+			first = new Node<>(element);
+			last = first;
+			++size;
+			return;
+		}
+		else if (index == size) {			//case: adding as last element
+			last.next = new Node<>(element);
+			last = last.next;
+			++size;
+			return;
+		}
+		//moving through the list
+		Node<E> current = first;
+		Node<E> previous = null;
+		int i = 0;
+		while (i != index) {	//No guard such as 'current != null' needed. Valid index range already tested.
+			previous = current;
+			current = current.next;
+			++i;
+		}
+		Node<E> n = new Node<>(element, current);
+		if (i == 0){					//case: not empty list, but adding as first element
+			first = n;
+		} else {						//case: adding in-between
+			previous.next = n;
+		}
+		last = n;
+		++size;
+	}
+
+	//deletion: [A]->[B]->[C]-| ; [A]->[C]
+	@Override
+	public E remove(int index) {
+		if (index >= size || index < 0) throw new IndexOutOfBoundsException("invalid index: too low or high");
+		if (size == 0)	throw new IndexOutOfBoundsException("empty list");			//case: empty list
+		//Note: 'size !=  0' is guaranteed from here on.
+		if (index == 0){ 				//case: get first element
+			Node<E> oldFirst = first;
+			first = first.next;
+			--size;
+			return oldFirst.elem;
+		}
+		//moving through the list
+		Node<E> current = first;
+		Node<E> previous = null;
+		int i = 0;
+		while (i != index) {	//No guard such as 'current != null' or 'i < size' needed. Valid index range already tested.
+			previous = current;
+			current = current.next;
+			++i;
+		}
+		previous.next = current.next; //removes element at index
+		if (i == size - 1) {			//case: get last element
+			//Note: previous.next will be automatically equal null.
+			// 	Reason: 'previous = current' && 'previous.next = current.next', current is last elem. and 'current.next == null'
+			last = previous; //last.next will be null because of 'previous.next == null';
+		}
+		--size;
+		return current.elem;
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] array = new Object[size];
+		int index = 0;
+		Node<E> current = first;
+		while (current != null) {
+			array[index++] = current.elem;
+			current = current.next;
+		}
+		return array;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+
+	private static class Node<E> {
+		private final E elem;
+		private Node<E> next = null;
+
+		private Node(E elem) {
+			this.elem = elem;
+		}
+
+		private Node(E elem, Node<E> next) {
+			this.elem = elem;
+			this.next = next;
+		}
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new MyIterator();
+	}
+
+	private class MyIterator implements Iterator<E> {
+		@Override
+		public boolean hasNext() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public E next() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	public static void main(String[] args) {
+		SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(Arrays.toString(list.toArray()));
+	}
+}
