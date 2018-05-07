5782576f764ee0cb1c3c2c34d9224943480127fc
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2018-05-07 10:33:17 +0200

    Initial commit

---
+++ 05-2_HeapSortDemo/src/ch/fhnw/algd/sortalgs/HeapSort.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 21.03.2014
+ */
+package ch.fhnw.algd.sortalgs;
+
+import ch.fhnw.algd.sortdemo.framework.SortAlg;
+import ch.fhnw.algd.sortdemo.framework.SortData;
+
+public class HeapSort implements SortAlg {
+	@Override
+	public void run(SortData data) {
+		//data: wird von Applikation drum herum zur Verfügung gestellt.
+
+		//Todo: data via Floyd zu einem MaxHeap, dann mit HeapSort sortieren.
+	}
+}
